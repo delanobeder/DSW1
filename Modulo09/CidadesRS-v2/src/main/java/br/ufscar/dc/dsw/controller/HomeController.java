1097d26b26cd4ff93b3636ac6cace26b486c1081
Delano Beder <delanobeder@yahoo.com.br>
2021-01-03 16:04:17 -0300

    Atualiza CidadesRS-v2

--- Modulo09/CidadesRS-v2/src/main/java/br/ufscar/dc/dsw/controller/HomeController.java
+++ Modulo09/CidadesRS-v2/src/main/java/br/ufscar/dc/dsw/controller/HomeController.java
@@ -1,10 +1,14 @@
 package br.ufscar.dc.dsw.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import br.ufscar.dc.dsw.domain.Cidade;
 import br.ufscar.dc.dsw.service.spec.ICidadeService;
 import br.ufscar.dc.dsw.service.spec.IEstadoService;
 
@@ -37,5 +41,18 @@ public class HomeController {
 	public String tabelaDinamica(ModelMap model) {
 		model.addAttribute("cidades", cidadeService.findAll());
 		return "tabelaDinamica";
+	}
+	
+	@GetMapping("/listaCidades")
+	public String listaCidades(@RequestParam(required = false) Long estado, ModelMap model) {
+		List<Cidade> cidades;
+		model.addAttribute("estados", estadoService.findAll());
+		if (estado == null) {
+			cidades = cidadeService.findAll();
+		} else {
+			cidades = cidadeService.findByEstado(estado);
+		}
+		model.addAttribute("cidades", cidades);
+		return "listaCidades";
 	}
 }
