5a0d2aa961d3d8c391957c9d4927e1a60d098579
Delano Beder <delanobeder@yahoo.com.br>
2023-05-15 19:14:31 -0300

    Alteração no exemplo Modulo01

--- Modulo01/Triade/script.js
+++ Modulo01/Triade/script.js
@@ -2,11 +2,25 @@
 function hideEven() {
     var flag = confirm('Você tem certeza?');
     if (flag) {
+        var b = document.getElementById("b1");
+        var hide = (b.textContent == "Hide Even");
+        console.log(hide);
         var divsToHide = document.getElementsByClassName("even");
         for (var i = 0; i < divsToHide.length; i++) {
-            divsToHide[i].style.visibility = "hidden"; 
-            divsToHide[i].style.display = "none";
+            if (hide) {
+                divsToHide[i].style.visibility = "hidden";
+                divsToHide[i].style.display = "none";
+            } else {
+                divsToHide[i].style.visibility = "visible";
+                divsToHide[i].style.display = "initial";
+            }
         }
+        if (hide) {
+            b.textContent = "Show Even";
+        } else {
+            b.textContent = "Hide Even";
+        }
+
     } else
         return false;
 
