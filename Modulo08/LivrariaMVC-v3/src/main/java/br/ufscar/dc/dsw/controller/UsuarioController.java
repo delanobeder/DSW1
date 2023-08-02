4e77036357a112b918031b2380fe19cd8d4d956d
Delano Beder <delanobeder@yahoo.com.br>
2023-08-02 13:47:54 -0300

    Permite a edição da senha (password) do Usuário

--- Modulo08/LivrariaMVC-v3/src/main/java/br/ufscar/dc/dsw/controller/UsuarioController.java
+++ Modulo08/LivrariaMVC-v3/src/main/java/br/ufscar/dc/dsw/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package br.ufscar.dc.dsw.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -58,15 +59,17 @@ public class UsuarioController {
 	}
 	
 	@PostMapping("/editar")
-	public String editar(@Valid Usuario usuario, BindingResult result, RedirectAttributes attr) {
+	public String editar(@Valid Usuario usuario, String novoPassword, BindingResult result, RedirectAttributes attr) {
 		
 		if (result.hasErrors()) {
 			return "usuario/cadastro";
 		}
 
-		System.out.println(usuario.getPassword());
-		usuario.setPassword(encoder.encode(usuario.getPassword()));
-
+		if (novoPassword != null && !novoPassword.trim().isEmpty()) {
+			usuario.setPassword(encoder.encode(novoPassword));
+		} else {
+			System.out.println("Senha não foi editada");
+		}
 		service.salvar(usuario);
 		attr.addFlashAttribute("sucess", "usuario.edit.sucess");
 		return "redirect:/usuarios/listar";
