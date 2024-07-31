2f4276ea2932b20b19212176fb470af4cae52fdf
Delano Medeiros Beder <delanobeder@yahoo.com.br>
2024-07-31 17:36:10 -0300

    Migração para especificação Servlet versão 6 (Módulo 03)

--- Modulo03/AloMundoJSP/src/main/java/br/ufscar/dc/dsw/AloMundoServlet.java
+++ Modulo03/AloMundoJSP/src/main/java/br/ufscar/dc/dsw/AloMundoServlet.java
@@ -2,11 +2,11 @@ package br.ufscar.dc.dsw;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"/AloMundoServlet"})
 public class AloMundoServlet extends HttpServlet {
